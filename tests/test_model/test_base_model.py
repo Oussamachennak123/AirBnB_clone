118f27b529b38ef7557c88629a8427d7f529486a
Oussama <chennak.oussama@gmail.com>
2024-03-10 21:23:06 +0100

    update

--- tests/test_model/test_base_model.py
+++ tests/test_model/test_base_model.py
@@ -7,14 +7,14 @@ from models.base_model import BaseModel
 class TestBaseModel(unittest.TestCase):
     def test_int(self):
         my_model = BaseModel()
-        
+
         self.assertIsNotNone(my_model.id)
         self.assertIsNotNone(my_model.created_at)
         self.assertIsNotNone(my_model.updated_at)
 
     def test_save(self):
         my_model = BaseModel()
-        
+
         init_updated_at = my_model.updated_at
         curr_updated_at = my_model.save()
         self.assertNotEqual(init_updated_at, curr_updated_at)
@@ -23,6 +23,20 @@ class TestBaseModel(unittest.TestCase):
         my_model = BaseModel()
         my_model_dct = my_model.to_dict()
         self.assertIsInstance(my_model_dct, dict)
-        
+
         self.assertEqual(my_model_dct["__class__"], 'BaseModel')
-        
+        self.assertEqual(my_model_dct['id'], my_model.id)
+        self.assertEqual(my_model_dct['created_at'],
+                         my_model.created_at.isoformat())
+        self.assertEqual(my_model_dct["updated_at"],
+                         my_model.created_at.isoformat())
+
+    def test_str(self):
+        my_model = BaseModel()
+        self.assertTrue(str(my_model).startswith('[BaseModel]'))
+        self.assertIn(my_model.id, str(my_model))
+        self.assertIn(str(my_model.__dict__), str(my_model))
+
+
+if __name__ == "__main__":
+    unittest.main()
