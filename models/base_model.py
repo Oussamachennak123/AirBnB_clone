a3cd8448df47c9d3d6ea8ca95eb71d1a705555f2
Oussama <chennak.oussama@gmail.com>
2024-04-06 12:06:38 +0000

    update

--- models/base_model.py
+++ models/base_model.py
@@ -7,53 +7,42 @@ import models
 
 
 class BaseModel:
-    def __init__(self, *arg, **kwargs):
-        tim_format = "%Y-%m-%dT%H:%M:%S.%f"
+    """Parent/base class. All other classes inherits from here."""
+
+    def __init__(self, *_args, **kwargs):
         if kwargs:
             for key1, value in kwargs.items():
-                if key1 == "__class__":
-                    continue
-                elif key1 == "created_at" or key1 == "updated_at":
-                    setattr(self, key1, datetime.strptime(value, tim_format))
+                if key1 == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f")
+                elif key1 == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f")
                 else:
-                    setattr(self, key1, value)
+                    self.__dict__[key1] = value
         else:
             self.id = str(uuid.uuid4())
-
-            self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
-        
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """A method to save attributes
+        """A method to save attributes of an instance
         """
-        self.updated_at = datetime.now
+        self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
-        """the key-paired values to dictionary
+        """hadles the key-paired values to dictionary
+
         Returns:
             dict: return dictionary
         """
-        key11_dict = {'__class__': self.__class__.__name__}
-        key11_dict.update({aa: bb.isoformat()
-            if isinstance(bb, datetime)
-            else bb for aa, bb in self.__dict__.items()})
-        return key11_dict
-
-
-if __name__ == "__main__":
-    my_model = BaseModel()
-    my_model.name = "My First Model"
-    my_model.my_number = 89
-    print(my_model)
-    my_model.save()
-    print(my_model)
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]),
-                                       my_model_json[key]))
+        cls_dict = {'__class__': self.__class__.__name__}
+        cls_dict.update({k: v.isoformat()
+                        if isinstance(v, datetime)
+                        else v for k, v in self.__dict__.items()})
+        return cls_dict
